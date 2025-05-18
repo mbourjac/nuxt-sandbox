b38a7de5c3cea1be0b5deb886d56487ca5708745
Michaël Bourjac <michael.bourjac@gmail.com>
2025-05-18 15:39:38 +0200

    chore: set up db

--- lib/env.ts
+++ lib/env.ts
@@ -3,6 +3,8 @@ import { tryParseEnv } from './try-parse-env';
 
 const envSchema = z.object({
   NODE_ENV: z.string(),
+  TURSO_DATABASE_URL: z.string(),
+  TURSO_AUTH_TOKEN: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
