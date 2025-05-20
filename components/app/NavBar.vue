c09086be3dcc6c7af27cd77f0f644c96c3856d22
Michaël Bourjac <michael.bourjac@gmail.com>
2025-05-20 19:31:29 +0200

    refactor(components): rename components using pascal case

--- components/app/NavBar.vue
+++ components/app/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="flex justify-between p-4">
     <NuxtLink to="/" class="flex items-center gap-2 font-medium">
-      <AppLogo />
+      <AppHomeLogo />
       <span>Travel Map</span>
     </NuxtLink>
     <div class="flex gap-2">
