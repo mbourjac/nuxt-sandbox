70341a4d61651e457b0777c183a19a179ed14aaf
Michaël Bourjac <michael.bourjac@gmail.com>
2025-05-21 06:44:59 +0200

    feat(home): add link to dashboard for signed in user

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="flex w-full items-center justify-center gap-12 p-4">
     <div
@@ -5,11 +9,16 @@
     />
     <div class="flex max-w-[30rem] flex-col gap-2">
       <h1 class="text-2xl font-medium">Travel Map</h1>
-      <p class="text-balance text-zinc-600 dark:text-zinc-300">
-        Keep track of your travels with this simple travel log app. Add
-        locations, photos, and notes to create a digital journal of your
-        journeys.
-      </p>
+      <div class="flex flex-col gap-4">
+        <p class="text-balance text-zinc-600 dark:text-zinc-300">
+          Keep track of your travels with this simple travel log app. Add
+          locations, photos, and notes to create a digital journal of your
+          journeys.
+        </p>
+        <Button v-if="authStore.user" as-child class="w-fit">
+          <NuxtLink to="/dashboard">Start logging</NuxtLink>
+        </Button>
+      </div>
     </div>
   </div>
 </template>
