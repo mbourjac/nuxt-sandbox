b028de72d7e264f9833f93a0ba2569f832231851
Michaël Bourjac <michael.bourjac@gmail.com>
2025-05-26 19:39:50 +0200

    feat(default-layout): update style

--- components/app/NavBar.vue
+++ components/app/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex justify-between p-4">
+  <nav class="flex justify-between px-6 py-[1.375rem]">
     <NuxtLink to="/" class="flex items-center gap-2 font-medium">
       <AppLogo />
       <span>Travel Map</span>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div class="bg-muted/50 flex min-h-screen flex-col">
     <AppNavBar />
-    <main class="flex flex-1">
-      <slot />
-    </main>
+    <div class="flex flex-1 p-4">
+      <main class="flex flex-1 rounded-xl bg-white p-4 dark:bg-black">
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
