4b47661878f21b388d3447749e76620b8f1a1f7d
Michaël Bourjac <michael.bourjac@gmail.com>
2025-05-26 19:38:45 +0200

    feat(dashboard): render nested dashboard pages

---
+++ pages/dashboard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dashboard',
+});
+</script>
+
+<template>
+  <div class="flex w-full p-4 pt-0">
+    <NuxtPage />
+  </div>
+</template>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-definePageMeta({
-  layout: 'dashboard',
-});
-</script>
-
 <template>
   <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
     <div class="grid auto-rows-min gap-4 md:grid-cols-3">
