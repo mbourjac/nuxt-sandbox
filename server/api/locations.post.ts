9a4ac5c1803f5715738770bc3a05db888631969e
Michaël Bourjac <michael.bourjac@gmail.com>
2025-05-27 20:49:02 +0200

    feat(add-location): add nanoid to slug if it already exists in the database

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,6 +1,10 @@
 import slug from 'slug';
 import { db } from '../../lib/db';
 import { insertLocationSchema, location } from '../../lib/db/schema';
+import { and, eq } from 'drizzle-orm';
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet('123456789abcdefghijklmnopqrstuvwxyz', 5);
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -34,12 +38,46 @@ export default defineEventHandler(async (event) => {
     );
   }
 
+  const existingLocation = await db.query.location.findFirst({
+    where: and(
+      eq(location.name, result.data.name),
+      eq(location.userId, event.context.user.id)
+    ),
+  });
+
+  if (existingLocation) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 409,
+        statusMessage: 'A location with that name already exists.',
+      })
+    );
+  }
+
+  let locationSlug = slug(result.data.name);
+  let existing = !!(await db.query.location.findFirst({
+    where: eq(location.slug, locationSlug),
+  }));
+
+  while (existing) {
+    const locationSlugWithNanoid = `${locationSlug}-${nanoid()}`;
+
+    existing = !!(await db.query.location.findFirst({
+      where: eq(location.slug, locationSlugWithNanoid),
+    }));
+
+    if (!existing) {
+      locationSlug = locationSlugWithNanoid;
+    }
+  }
+
   try {
     const [created] = await db
       .insert(location)
       .values({
         ...result.data,
-        slug: slug(result.data.name),
+        slug: locationSlug,
         userId: event.context.user.id,
       })
       .returning();
